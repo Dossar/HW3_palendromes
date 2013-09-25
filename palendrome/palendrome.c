89b7e259e584e18113e6321db5824e0f272a92ce
mikemeding <mikeymeding@gmail.com>
2013-09-25 08:34:07 -0400

    added some comments to palendrome code

--- palendrome/palendrome.c
+++ palendrome/palendrome.c
@@ -11,29 +11,39 @@
 
 int main(int argc, char** argv) {
 
-    char text[100];
-    int begin, middle, end, length = 0;
+   // Variables
+   char text[100];
+   int begin, middle, end, length = 0;
 
-    gets(text);
+   // Input text into array
+   gets(text);
 
-    while (text[length] != '\0')
-        length++;
+   // get the current length of the array by iterating until the null terminating character
+   while (text[length] != '\0')
+	  length++;
 
-    end = length - 1;
-    middle = length / 2;
+   // set end to be one less than the length because it goes from 0 - (length -1)
+   end = length - 1;
+   // middle is obviously half of the length
+   middle = length / 2;
 
-    for (begin = 0; begin < middle; begin++) {
-        if (text[begin] != text[end]) {
-            printf("Not a palendrome.\n");
-            break;
-        }
-        end--;
-    }
-    if (begin == middle) {
-        printf("Palendrome.\n");
-    }
+   // iterate though all of the letters from the beginning to the middle of the string
+   for (begin = 0; begin < middle; begin++) {
+	  // if the text at the current position and the the (end - the current position) it is not a palendrome
+	  if (text[begin] != text[end]) {
+		 printf("Not a palendrome.\n");
+		 break; // in which case break out of the loop
+	  }
+	  end--;
+   }
+
+   // checks to see if the loop ended sucessfully finding a palendrome.
+   // if a palendrome is found then that means that the loop sucessfully iterated to the center of the word
+   if (begin == middle) {
+	  printf("Palendrome.\n");
+   }
 
 
-    return (EXIT_SUCCESS);
+   return (EXIT_SUCCESS); // end of program
 }
 
